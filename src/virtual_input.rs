e9187a428fd98c541aa58825178716d1e0e152b5
T4rp <why9ten@gmail.com>
2025-05-16 06:54:03 -0400

    style: Be explicit with all types in ffi code
    progrom sometimes segfaults when i start the autoclicker

--- src/virtual_input.rs
+++ src/virtual_input.rs
@@ -1,13 +1,20 @@
-use std::{ffi::c_void, io};
+use std::{io, os::raw};
 
-use crate::uinput::{self, UI_DEV_CREATE, UI_DEV_SETUP, input_event};
+use crate::uinput::{
+    self, __s32, __suseconds_t, __time_t, __u16, __u32, UI_DEV_CREATE, UI_DEV_SETUP, input_event,
+};
 use libc::{O_NONBLOCK, O_WRONLY};
 
-fn emit_input_event(fd: i32, t: u16, code: u16, val: i32) -> io::Result<libc::ssize_t> {
+fn emit_input_event(
+    fd: libc::c_int,
+    t: __u16,
+    code: __u16,
+    val: __s32,
+) -> io::Result<libc::ssize_t> {
     let ie = uinput::input_event {
         time: uinput::timeval {
-            tv_sec: 0,
-            tv_usec: 0,
+            tv_sec: 0 as __time_t,
+            tv_usec: 0 as __suseconds_t,
         },
         type_: t,
         code,
@@ -17,7 +24,7 @@ fn emit_input_event(fd: i32, t: u16, code: u16, val: i32) -> io::Result<libc::ss
     unsafe {
         match libc::write(
             fd,
-            &ie as *const uinput::input_event as *const c_void,
+            &ie as *const uinput::input_event as *const libc::c_void,
             std::mem::size_of::<input_event>(),
         ) {
             -1 => Err(io::Error::last_os_error()),
@@ -26,7 +33,7 @@ fn emit_input_event(fd: i32, t: u16, code: u16, val: i32) -> io::Result<libc::ss
     }
 }
 
-fn ioctl_int_arg(fd: libc::c_int, op: libc::c_ulong, arg: libc::c_int) -> io::Result<()> {
+fn ioctl_int_arg(fd: raw::c_int, op: raw::c_ulong, arg: raw::c_int) -> io::Result<()> {
     unsafe {
         if uinput::ioctl(fd, op, arg) == -1 {
             Err(io::Error::last_os_error())
@@ -36,7 +43,7 @@ fn ioctl_int_arg(fd: libc::c_int, op: libc::c_ulong, arg: libc::c_int) -> io::Re
     }
 }
 
-fn ioctl_no_arg(fd: libc::c_int, op: libc::c_ulong) -> io::Result<()> {
+fn ioctl_no_arg(fd: raw::c_int, op: raw::c_ulong) -> io::Result<()> {
     unsafe {
         if uinput::ioctl(fd, op) == -1 {
             Err(io::Error::last_os_error())
@@ -46,7 +53,7 @@ fn ioctl_no_arg(fd: libc::c_int, op: libc::c_ulong) -> io::Result<()> {
     }
 }
 
-fn ioctl_ptr_arg(fd: libc::c_int, op: libc::c_ulong, arg: *const c_void) -> io::Result<()> {
+fn ioctl_ptr_arg(fd: raw::c_int, op: raw::c_ulong, arg: *const raw::c_void) -> io::Result<()> {
     unsafe {
         if uinput::ioctl(fd, op, arg) == -1 {
             Err(io::Error::last_os_error())
@@ -69,7 +76,7 @@ fn open_uinput() -> Result<libc::c_int, io::Error> {
 }
 
 pub struct VirtualMouse {
-    fd: i32,
+    fd: libc::c_int,
 }
 
 #[allow(unused)]
@@ -78,35 +85,54 @@ impl VirtualMouse {
         let fd = unsafe {
             let fd = open_uinput()?;
 
-            ioctl_int_arg(fd, uinput::UI_SET_EVBIT as u64, uinput::EV_KEY as i32)?;
-            ioctl_int_arg(fd, uinput::UI_SET_KEYBIT as u64, uinput::BTN_LEFT as i32)?;
-            ioctl_int_arg(fd, uinput::UI_SET_KEYBIT as u64, uinput::BTN_RIGHT as i32)?;
-            ioctl_int_arg(fd, uinput::UI_SET_KEYBIT as u64, uinput::BTN_MIDDLE as i32)?;
+            ioctl_int_arg(
+                fd,
+                uinput::UI_SET_EVBIT as raw::c_ulong,
+                uinput::EV_KEY as raw::c_int,
+            )?;
 
-            let mut name: [i8; 80] = [0; 80];
+            ioctl_int_arg(
+                fd,
+                uinput::UI_SET_KEYBIT as raw::c_ulong,
+                uinput::BTN_LEFT as raw::c_int,
+            )?;
+
+            ioctl_int_arg(
+                fd,
+                uinput::UI_SET_KEYBIT as raw::c_ulong,
+                uinput::BTN_RIGHT as raw::c_int,
+            )?;
+
+            ioctl_int_arg(
+                fd,
+                uinput::UI_SET_KEYBIT as raw::c_ulong,
+                uinput::BTN_MIDDLE as raw::c_int,
+            )?;
+
+            let mut name: [raw::c_char; 80] = [0; 80];
 
             for (i, &b) in b"VirtualMouse".iter().enumerate() {
-                name[i] = b as i8;
+                name[i] = b as raw::c_char;
             }
 
             let usetup = uinput::uinput_setup {
                 id: uinput::input_id {
-                    bustype: uinput::BUS_USB as u16,
-                    vendor: 0x1337,
-                    product: 0x7331,
-                    version: 0,
+                    bustype: uinput::BUS_USB as __u16,
+                    vendor: 0x1337 as __u16,
+                    product: 0x7331 as __u16,
+                    version: 0 as __u16,
                 },
                 name,
-                ff_effects_max: 0,
+                ff_effects_max: 0 as __u32,
             };
 
             ioctl_ptr_arg(
-                fd,
-                UI_DEV_SETUP as u64,
-                &usetup as *const _ as *const c_void,
+                fd as raw::c_int,
+                UI_DEV_SETUP as raw::c_ulong,
+                &usetup as *const _ as *const raw::c_void,
             )?;
 
-            ioctl_no_arg(fd, UI_DEV_CREATE as u64)?;
+            ioctl_no_arg(fd, UI_DEV_CREATE as raw::c_ulong)?;
 
             fd
         };
@@ -115,18 +141,65 @@ impl VirtualMouse {
     }
 
     pub fn left_click(&self) -> io::Result<()> {
-        emit_input_event(self.fd, uinput::EV_KEY as u16, uinput::BTN_LEFT as u16, 1)?;
-        emit_input_event(self.fd, uinput::EV_SYN as u16, uinput::SYN_REPORT as u16, 0)?;
-        emit_input_event(self.fd, uinput::EV_KEY as u16, uinput::BTN_LEFT as u16, 0)?;
-        emit_input_event(self.fd, uinput::EV_SYN as u16, uinput::SYN_REPORT as u16, 0)?;
+        emit_input_event(
+            self.fd as raw::c_int,
+            uinput::EV_KEY as __u16,
+            uinput::BTN_LEFT as __u16,
+            1 as raw::c_int,
+        )?;
+
+        emit_input_event(
+            self.fd as raw::c_int,
+            uinput::EV_SYN as __u16,
+            uinput::SYN_REPORT as __u16,
+            0 as raw::c_int,
+        )?;
+
+        emit_input_event(
+            self.fd as raw::c_int,
+            uinput::EV_KEY as __u16,
+            uinput::BTN_LEFT as __u16,
+            0 as raw::c_int,
+        )?;
+
+        emit_input_event(
+            self.fd as raw::c_int,
+            uinput::EV_SYN as __u16,
+            uinput::SYN_REPORT as __u16,
+            0 as raw::c_int,
+        )?;
+
         Ok(())
     }
 
     pub fn right_click(&self) -> io::Result<()> {
-        emit_input_event(self.fd, uinput::EV_KEY as u16, uinput::BTN_RIGHT as u16, 1)?;
-        emit_input_event(self.fd, uinput::EV_SYN as u16, uinput::SYN_REPORT as u16, 0)?;
-        emit_input_event(self.fd, uinput::EV_KEY as u16, uinput::BTN_RIGHT as u16, 0)?;
-        emit_input_event(self.fd, uinput::EV_SYN as u16, uinput::SYN_REPORT as u16, 0)?;
+        emit_input_event(
+            self.fd as raw::c_int,
+            uinput::EV_KEY as __u16,
+            uinput::BTN_RIGHT as __u16,
+            1 as raw::c_int,
+        )?;
+
+        emit_input_event(
+            self.fd as raw::c_int,
+            uinput::EV_SYN as __u16,
+            uinput::SYN_REPORT as __u16,
+            0 as raw::c_int,
+        )?;
+
+        emit_input_event(
+            self.fd as raw::c_int,
+            uinput::EV_KEY as __u16,
+            uinput::BTN_RIGHT as __u16,
+            0 as raw::c_int,
+        )?;
+
+        emit_input_event(
+            self.fd as raw::c_int,
+            uinput::EV_SYN as __u16,
+            uinput::SYN_REPORT as __u16,
+            0 as raw::c_int,
+        )?;
 
         Ok(())
     }
