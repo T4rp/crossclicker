fe635538ab58e05c645a22fc7663462cc83a23e1
T4rp <why9ten@gmail.com>
2025-05-17 19:40:20 -0400

    more futile attempts to fix the segfault

--- src/virtual_input.rs
+++ src/virtual_input.rs
@@ -1,55 +1,30 @@
-use std::{io, os::raw};
+use std::{io, mem, os::raw};
 
 use crate::uinput::{
-    self, __s32, __suseconds_t, __time_t, __u16, __u32, EV_KEY, EV_SYN, SYN_REPORT, UI_DEV_CREATE,
-    UI_DEV_SETUP, input_event,
+    self, __s32, __u16, __u32, __u64, BTN_LEFT, EV_KEY, EV_SYN, SYN_REPORT, UI_DEV_CREATE,
+    UI_DEV_DESTROY, UI_DEV_SETUP,
 };
-use libc::{O_NONBLOCK, O_WRONLY};
+use libc::{O_NONBLOCK, O_WRONLY, ssize_t};
 
-fn emit_key_event(fd: libc::c_int, code: __u16, val: __s32) -> io::Result<()> {
-    let key_event = uinput::input_event {
-        time: uinput::timeval {
-            tv_sec: 0 as __time_t,
-            tv_usec: 0 as __suseconds_t,
-        },
-        type_: EV_KEY as __u16,
-        code: code as __u16,
-        value: val as __s32,
-    };
-
-    let sync_event = uinput::input_event {
-        time: uinput::timeval {
-            tv_sec: 0 as __time_t,
-            tv_usec: 0 as __suseconds_t,
-        },
-        type_: EV_SYN as __u16,
-        code: SYN_REPORT as __u16,
-        value: 0 as __s32,
-    };
-
+fn emit_event(fd: libc::c_int, t: __u16, code: __u16, val: __s32) -> io::Result<ssize_t> {
     unsafe {
-        let res = libc::write(
-            fd,
-            &key_event as *const _ as *const libc::c_void,
-            std::mem::size_of::<input_event>() as libc::size_t,
-        );
+        #[allow(invalid_value)]
+        let mut input_event: uinput::input_event = mem::MaybeUninit::uninit().assume_init();
+        input_event.type_ = t;
+        input_event.code = code;
+        input_event.value = val;
+        input_event.time.tv_sec = 0;
+        input_event.time.tv_usec = 0;
 
-        if res < 0 {
-            return Err(io::Error::last_os_error());
+        match libc::write(
+            fd as libc::c_int,
+            &input_event as *const _ as *const libc::c_void,
+            size_of::<uinput::input_event>() as libc::size_t,
+        ) {
+            -1 => Err(io::Error::last_os_error()),
+            r => Ok(r),
         }
-
-        let res = libc::write(
-            fd,
-            &sync_event as *const _ as *const libc::c_void,
-            std::mem::size_of::<input_event>() as libc::size_t,
-        );
-
-        if res < 0 {
-            return Err(io::Error::last_os_error());
-        }
-    };
-
-    Ok(())
+    }
 }
 
 fn ioctl_int_arg(fd: raw::c_int, op: raw::c_ulong, arg: raw::c_int) -> io::Result<()> {
@@ -107,13 +82,13 @@ impl VirtualMouse {
             ioctl_int_arg(
                 fd,
                 uinput::UI_SET_EVBIT as raw::c_ulong,
-                uinput::EV_KEY as raw::c_int,
+                EV_KEY as raw::c_int,
             )?;
 
             ioctl_int_arg(
                 fd,
                 uinput::UI_SET_KEYBIT as raw::c_ulong,
-                uinput::BTN_LEFT as raw::c_int,
+                BTN_LEFT as raw::c_int,
             )?;
 
             let mut name: [raw::c_char; 80] = [0; 80];
@@ -148,34 +123,43 @@ impl VirtualMouse {
     }
 
     pub fn left_click(&self) -> io::Result<()> {
-        emit_key_event(
+        emit_event(
             self.fd as raw::c_int,
-            uinput::BTN_LEFT as __u16,
-            1 as raw::c_int,
+            EV_KEY as __u16,
+            BTN_LEFT as __u16,
+            1 as __s32,
         )?;
 
-        emit_key_event(
+        emit_event(
             self.fd as raw::c_int,
-            uinput::BTN_LEFT as __u16,
-            0 as raw::c_int,
+            EV_SYN as __u16,
+            SYN_REPORT as __u16,
+            0 as __s32,
+        )?;
+
+        emit_event(
+            self.fd as raw::c_int,
+            EV_KEY as __u16,
+            BTN_LEFT as __u16,
+            0 as __s32,
+        )?;
+
+        emit_event(
+            self.fd as raw::c_int,
+            EV_SYN as __u16,
+            SYN_REPORT as __u16,
+            0 as __s32,
         )?;
 
         Ok(())
     }
+}
 
-    pub fn right_click(&self) -> io::Result<()> {
-        emit_key_event(
-            self.fd as raw::c_int,
-            uinput::BTN_RIGHT as __u16,
-            1 as raw::c_int,
-        )?;
-
-        emit_key_event(
-            self.fd as raw::c_int,
-            uinput::BTN_RIGHT as __u16,
-            0 as raw::c_int,
-        )?;
-
-        Ok(())
+impl Drop for VirtualMouse {
+    fn drop(&mut self) {
+        unsafe {
+            uinput::ioctl(self.fd, UI_DEV_DESTROY as __u64);
+            libc::close(self.fd);
+        };
     }
 }
